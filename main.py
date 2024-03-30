a37b26edbbcf0431275df261702db7a8dc3c613b
nikitaperdel <nikitaperdel@gmail.com>
2024-03-30 17:26:17 +0300

    был сделан задний фон и отображение окна и еще раз

---
+++ main.py
@@ -0,0 +1,64 @@
+import pygame
+from pygame import *
+from random import randint
+
+font.init()
+font_text = font.Font(None, 36)
+
+WIDTH = 600
+HEIGHT = 500
+FPS = 60
+WIN_SCORE = 10
+RESTART_TIME = 3000
+
+def gencolor():
+    return (randint(0, 255), randint(0, 255), randint(0, 255))
+
+background = gencolor()
+window = display.set_mode((WIDTH, HEIGHT))
+display.set_caption("Pingpong2DEngine")
+clock = time.Clock()
+
+class GameSprite(sprite.Sprite):
+    def __init__(self, p_image: str, x: int, y: int, h: int, w: int, speed: int):
+        super().__init__()
+        self.image = transform.scale(image.load(p_image), (w,h))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.speed = speed
+    def reset(self):
+        window.blit(self.image, (self.rect.x , self.rect.y))
+
+class Player(GameSprite):
+    def __init__(self, p_image: str, x: int, y: int, h: int, w: int, speed: int):
+        super().__init__(p_image, x, y, h, w, speed)
+
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_a] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_d] and self.rect.y < WIDTH - 80:
+            self.rect.y += self.speed
+
+class Player2(GameSprite):
+    def __init__(self, p_image: str, x: int, y: int, h: int, w: int, speed: int):
+        super().__init__(p_image, x, y, h, w, speed)
+
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_a] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_d] and self.rect.y < WIDTH - 80:
+            self.rect.y += self.speed
+
+color_sel = False
+Engine = True
+
+while Engine:
+    window.fill(background)
+    for e in event.get():
+        if e.type == QUIT:
+            Engine = False  
+    display.update()
+    clock.tick(FPS)
